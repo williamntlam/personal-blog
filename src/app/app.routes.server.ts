621b1a300e2b5c70e2f8a10c07afe80c90679536
William Lam <williamntlam@gmail.com>
2025-09-14 18:49:57 -0400

    fix: fixed SSR rendering

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,17 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'blog/:id',
+    renderMode: RenderMode.Prerender,
+    getPrerenderParams: () => Promise.resolve([
+      { id: '1' },
+      { id: '2' },
+      { id: '3' },
+      { id: '4' },
+      { id: '5' }
+    ])
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
