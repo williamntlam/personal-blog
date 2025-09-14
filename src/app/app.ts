e63d9e2d6a63c6ec20144dd729f9da479345e738
William Lam <williamntlam@gmail.com>
2025-09-13 22:34:23 -0400

    feat: implemented minimalistic home page

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,27 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, signal, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit {
   protected readonly title = signal('personal-blog');
+  protected isDarkMode = true;
+
+  ngOnInit() {
+    // Set dark mode by default
+    document.documentElement.classList.add('dark');
+  }
+
+  toggleTheme() {
+    this.isDarkMode = !this.isDarkMode;
+    if (this.isDarkMode) {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+  }
 }
